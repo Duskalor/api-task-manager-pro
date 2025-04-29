f2035fef67b77ef97223b3a757965b75375ad46d
duskalor <duskalor1@gmail.com>
2025-04-29 00:51:39 -0500

    QA

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,16 +1,58 @@
-import type { Request } from 'express';
-import { createUser, userSchema } from 'models/user';
+import type { Request, Response } from 'express';
+import type { AuthenticatedRequest } from 'middelware/authentication';
+import { addRevokeToken } from 'models/revoke-token';
+import {
+  createUser,
+  getUserByEmail,
+  revokeUserToken,
+  userSchema,
+} from 'models/user';
 import { parse } from 'valibot';
 
-export const userController = async (req: Request, res: Response) => {
-  const body = req.body;
-  try {
-    const result = parse(userSchema, body);
-    const user = await createUser(result.email, result.password);
-    res.status(201).json({
-      data: user,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+export const userController = {
+  async addUser(req: Request, res: Response) {
+    const body = req.body;
+    try {
+      const result = parse(userSchema, body);
+      const user = await createUser(result.email, result.password);
+      res.status(201).json({
+        data: user,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async getUser(req: Request, res: Response) {
+    const { email } = req.body;
+    if (!email) {
+      return res.status(400).json({ message: 'Email is required' });
+    }
+    try {
+      const user = await getUserByEmail(email);
+      res.status(200).json({
+        data: user,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async logout(req: Request, res: Response) {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (token) {
+      addRevokeToken(token);
+      const formattedReq = req as AuthenticatedRequest;
+      if (
+        formattedReq.user &&
+        typeof formattedReq.user === 'object' &&
+        'id' in formattedReq.user
+      ) {
+        const result = await revokeUserToken(formattedReq.user.email);
+
+        if (!result) {
+          return res.status(401).json({ message: 'Forbidden' });
+        }
+        return res.status(200).json({ message: 'Logged out successfully' });
+      }
+    }
+  },
 };
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,8 @@
+import { userController } from 'controller/user-controller';
 import { Router } from 'express';
 
 export const router = Router();
 
-router.post('/login', async (req, res) => {});
+router.post('/login', userController.addUser);
+router.post('/register', userController.addUser);
+router.post('/logout', userController.addUser);
